50337af3c6fd9de1891646ed95caf41ff973a60d
John Till <johndtill@gmail.com>
2020-07-02 08:15:29 -0400

    Aesthetic changes

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Perfect hash generator - brute force search to find a good hash function.
 
 ![screenshot](screenshot.png?raw=true "Poifect")
 
-<!--[Try it in your browser!](https://johndtill.github.io/Poifect/)-->
-
 ## Critical Reviews
 
 **★★★★★**<br>
@@ -23,3 +21,9 @@ Perfect hash generator - brute force search to find a good hash function.
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Credits
+
+Porpoise image by [Clker-Free-Vector-Images](https://pixabay.com/users/Clker-Free-Vector-Images-3736) from [Pixabay](https://pixabay.com).
+
+![Poirpose](src/dolphin-310228.svg?raw=true "A Poifect Porpoise")
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -22,17 +22,10 @@ static uint8_t progress = 0;
 static bool terminated = true;
 static std::string msg;
 
-#ifdef __EMSCRIPTEN__ //WASM code isn't updating correctly
-#include <emscripten/emscripten.h>
-#endif
-
 static void callback(){
     main_window->ui->progressBar->setValue(progress);
     main_window->ui->statusLabel->setText(QString::fromStdString(msg));
     QCoreApplication::processEvents();
-    #ifdef __EMSCRIPTEN__ //WASM code isn't updating correctly
-    emscripten_sleep(1);
-    #endif
 }
 
 void MainWindow::writeHashFunction(){
@@ -52,7 +45,6 @@ void MainWindow::writeHashFunction(){
     }
     QChar delimiter = delimiter_str.front();
 
-    #ifndef __EMSCRIPTEN__ //WASM code isn't splitting correctly
     QVector<QStringRef> rows = ui->inputEdit->toPlainText().splitRef('\n');
     if(rows.size() > 1 && rows.back().isEmpty()) rows.pop_back();
 
@@ -84,39 +76,6 @@ void MainWindow::writeHashFunction(){
         keys[i] = key;
         vals[i] = entries[1].toString().toStdString();
     }
-    #else
-    auto rows = ui->inputEdit->toPlainText().split('\n');
-    if(rows.size() > 1 && rows.back().isEmpty()) rows.pop_back();
-
-    std::vector<std::string> keys(static_cast<std::vector<std::string>::size_type>(rows.size()));
-    std::vector<std::string> vals(static_cast<std::vector<std::string>::size_type>(rows.size()));
-
-    for(std::vector<std::string>::size_type i = 0; i < keys.size(); i++){
-        auto entries = rows[static_cast<QVector<QStringRef>::size_type>(i)].split(delimiter);
-        if(entries.size() < 2){
-            if(entries.size()==0){
-                ui->outputEdit->setText("Row " + QString::number(i+1) + " NO_ENTRY, rows " + QString::number(rows.size()));
-                return;
-            }
-
-            ui->outputEdit->setText("Row " + QString::number(i+1) + " does not have 2 entries, rows " + QString::number(rows.size())
-                                    + "\n" + entries.front());
-            return;
-        }
-
-        std::string key = entries[0].toStdString();
-        for(std::vector<std::string>::size_type j = 0; j < i; j++){
-            if(keys[j] == key){
-                ui->outputEdit->setText("Key on row " + QString::number(i+1) +
-                                        " duplicates key on row " + QString::number(j+1));
-                return;
-            }
-        }
-
-        keys[i] = key;
-        vals[i] = entries[1].toStdString();
-    }
-    #endif
 
     terminated = false;
     QString old_btn_msg = ui->pushButton->text();
